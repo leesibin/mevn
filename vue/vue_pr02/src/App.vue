a3677e2f0487f51b807b675e329b684e16d50bff
leesibin <tlqls123@gmail.com>
2022-12-23 17:49:44 +0900

    12/23 오후 수업마무리

---
+++ vue/vue_pr02/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h3>{{inData}}</h3>
+    <input type="text" v-model="inData">
+    <button v-on:click ='add'>추가</button>
+    <button v-on:click ='status' >삭제</button>
+   
+      <div v-for="data in inData" :key='data'>{{data}}</div>
+    
+    <p>추가{{aput}}</p>
+  
+
+</template>
+
+<script>
+export default{
+  name : 'app',
+  data(){
+    return{
+    inData : '',
+    status : false,
+   aput:''
+    }
+    },
+  add:function(){
+    this.inData.push(this.inData.length+1)
+    this.aput = this.inData.length
+  }
+}
+
+ 
+
+
+
+
+</script>
+
+<style>
+
+</style>

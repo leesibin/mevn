1e13797e7117254df42de5b98765b8f028da6518
leesibin <tlqls123@gmail.com>
2022-12-27 17:24:09 +0900

    12/27 오후수업 중간저장

--- vue/vue_pr02/src/App.vue
+++ vue/vue_pr02/src/App.vue
@@ -1,37 +1,50 @@
 <template>
-  <h3>{{inData}}</h3>
-    <input type="text" v-model="inData">
-    <button v-on:click ='add'>추가</button>
-    <button v-on:click ='status' >삭제</button>
-   
-      <div v-for="data in inData" :key='data'>{{data}}</div>
-    
-    <p>추가{{aput}}</p>
-  
-
+  <h3>구구단</h3>
+  <input type="text" v-model="gugu" v-on:keyup.enter="bdd()">
+  <button v-on:click="bdd">열려라 참깨!</button>
+  <button v-on:click="bub">닫아라 참깨!</button>
+  <br>
+  <span v-for="data0 in guguData" :key="data0">{{ data0 }} </span>
+  <p>몇단이죠?{{ guguanum }}</p>
 </template>
 
 <script>
-export default{
-  name : 'app',
-  data(){
-    return{
-    inData : '',
-    status : false,
-   aput:''
+export default {
+  name: 'app',
+  data() {
+    return {
+      gugu: '',
+      guguData: [],
+      guguanum: '',
+
     }
+  },
+  
+  methods: {
+    func: function (text) {
+      alert(text)
     },
-  add:function(){
-    this.inData.push(this.inData.length+1)
-    this.aput = this.inData.length
+    bdd: function () {
+      this.guguData.push(this.gugu*1 +'x1 = '+this.gugu * 1,
+      this.gugu*1 +'x2 = '+this.gugu * 2+',',
+      this.gugu*1 +'x3 = '+this.gugu * 3,
+      this.gugu*1 +'x4 = '+this.gugu * 4,
+      this.gugu*1 +'x5 = '+this.gugu * 5,
+      this.gugu*1 +'x6 = '+this.gugu * 6,
+      this.gugu*1 +'x7 = '+this.gugu * 7,
+      this.gugu*1 +'x8 = '+this.gugu * 8,
+      this.gugu*1 +'x9 = '+this.gugu * 9)
+      this.guguanum = this.gugu + '단'
+      this.gugu = ''
+    },
+    bub: function () {
+      this.gugu = ''
+      this.guguData.splice(0);
+      this.guguanum = this.guguData.splice(0);
+
+    }
   }
 }
-
- 
-
-
-
-
 </script>
 
 <style>

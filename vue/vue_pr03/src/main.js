50b2a03f419960d7809a6037dbe24598a19479ad
leesibin <tlqls123@gmail.com>
2023-01-02 17:50:19 +0900

    01/02 오후 수업 마무리

--- vue/vue_pr03/src/main.js
+++ vue/vue_pr03/src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import socket from 'vue-socket.io'
+
+import io from 'socket.io-client'
+const socket = io('http://localhost:8080', { transports: ['websocket'] })
 
 const app = createApp(App)
-
-app.config.globalProperties.socket = socket
+app.config.globalProperties.$socket = socket
 
 app.use(store).use(router).mount('#app')
--- vue/vue_pr03/src/views/chat.vue
+++ vue/vue_pr03/src/views/chat.vue
@@ -1,11 +1,4 @@
 <template>
-  <div>
-    <h2>로그인</h2>
-
-    닉네임<input type="text" v-model="name" /><br />
-    메세지<input type="text" v-model="meg" v-on:keyup.enter="add" />
-    <button @click="add">전송</button>
-  </div>
   <div id="container">
     안녕하세요. 채팅창을 만들고 있는 중입니다...
     <div>
@@ -13,37 +6,66 @@
         <li v-for="data in arrmeg" :key="data">{{ data }}</li>
       </ul>
     </div>
-    <div></div>
+    <div>
+      <input type="text" v-model="state" v-on:keyup.enter="add" />
+      <button @click="add">전송</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { getCurrentInstance, onMounted, reactive } from '@vue/runtime-core'
+
 export default {
-  name: 'app',
+  name: 'App',
   data() {
     return {
-      naem: '',
+      inmeg: '',
       arrmeg: [],
       meg: '',
       megtext: ''
     }
   },
-  methods: {
-    add: function (message) {
-      this.arrmeg.push(this.name, this.meg)
-      this.megtext = this.arrmeg
-      this.meg = ''
-      this.socket.on('SEND_MESSAGE', { message })
+
+  setup() {
+    const app = getCurrentInstance()
+    const $socket = app.appContext.config.globalProperties.$socket
+
+    const state = reactive({
+      message: '',
+      textarea: ''
+    })
+
+    function chatSubmit() {
+      $socket.emit('chat', {
+        message: state.message
+      })
+      state.message = ''
     }
 
-    // setData: function () {
-    //   if (this.inValue) {
-    //     // this.text_save.push(this.inValue)
-    //     this.temp = this.inValue
-    //   }
-    // }
+    onMounted(() => {
+      $socket.on('chat', (data) => {
+        state.textarea += data.message + '\n'
+      })
+    })
+
+    return {
+      state,
+      chatSubmit
+    }
+  },
+
+  methods: {
+    add: function () {
+      this.arrmeg.push(this.state)
+      this.megtext = this.arrmeg
+    }
   }
 }
 </script>
 
-<style></style>
+<style>
+li {
+  list-style-type: none;
+}
+</style>

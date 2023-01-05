2d8e8f8d2efd55b5f485533f4b23496c6ced2f4c
leesibin <tlqls123@gmail.com>
2023-01-05 12:40:55 +0900

    1/05 서버 저장하기

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
